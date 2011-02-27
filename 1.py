67f5dfc9a9405e03f009820a33a36c558a4818aa
Florian Mayer <flormayer@aim.com>
2011-02-27 11:54:44 +0100

    Improve names.

--- 1.py
+++ 1.py
@@ -1,5 +1,5 @@
 def main():
-    return sum(y for y in xrange(1000) if any(not y % x for x in [3, 5]))
+    return sum(num for num in xrange(1000) if any(not num % x for x in [3, 5]))
 
 
 if __name__ == '__main__':
