8400eeb760f260d566d81a96f03918e30769db10
Florian Mayer <flormayer@aim.com>
2011-02-27 11:51:50 +0100

    Simplify code.

--- 1.py
+++ 1.py
@@ -1,12 +1,5 @@
-from itertools import ifilter
-
-
-def is_multiple_of_three_or_five(num):
-    return any(num % x == 0 for x in [3, 5])
-
-
 def main():
-    return sum(ifilter(is_multiple_of_three_or_five, xrange(1000)))
+    return sum(y for y in xrange(1000) if any(not y % x for x in [3, 5]))
 
 
 if __name__ == '__main__':
